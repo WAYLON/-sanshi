e1c0d038b6b946be2b9a623255853938ded6382b
wanglei <cn_wlei@163.com>
2020-08-19 10:08:18 +0800

    修改sass为node-sass，避免el-icon图标乱码

--- sanshi-ui/src/utils/sanshi.js
+++ sanshi-ui/src/utils/sanshi.js
@@ -131,7 +131,7 @@ export function handleTree(data, id, parentId, children, rootId) {
 	id = id || 'id'
 	parentId = parentId || 'parentId'
 	children = children || 'children'
-	rootId = rootId || 0
+  rootId = rootId || Math.min.apply(Math, data.map(item => { return item[parentId] })) || 0
 	//对源数据深度克隆
 	const cloneData = JSON.parse(JSON.stringify(data))
 	//循环所有项
@@ -139,7 +139,7 @@ export function handleTree(data, id, parentId, children, rootId) {
 		let branchArr = cloneData.filter(child => {
 			//返回每一项的子级数组
 			return father[id] === child[parentId]
-		});
+		});n
 		branchArr.length > 0 ? father.children = branchArr : '';
 		//返回第一层
 		return father[parentId] === rootId;
